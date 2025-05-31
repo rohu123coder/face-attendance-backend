08b2378df9e53e2ebba0b3aab4254113f51c0b34
rohu123coder <rohit.1702jha@gmail.com>
2025-05-31 21:02:27 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,17 +1,16 @@
-const mysql = require('mysql2/promise'); // use promise pool
-require('dotenv').config();
+const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  ssl: {
-    rejectUnauthorized: true, // this enables SSL
-  },
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
+  ssl: {
+    rejectUnauthorized: true  // 💡 Important: Enables secure connection
+  }
 });
 
 module.exports = pool;
